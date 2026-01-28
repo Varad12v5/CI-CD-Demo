18e1b39b3f6493652d65c46b9e348633c7bbe113
Varad Sachin Brahmapurkar <vbrahmapurkar@gmail.com>
2026-01-28 15:29:08 +0530

    Fix CI pipeline for master branch

--- src/main/java/com/example/cicd/Controller/HelloController.java
+++ src/main/java/com/example/cicd/Controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.cicd.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
     @GetMapping("/")
